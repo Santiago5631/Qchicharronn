5e4163e9de3db21e69d3fdfabc4bd88a60597dd7
David Puerto <puertobotiadavidalejandromail.com>
2025-10-09 07:48:21 -0500

    aplicaciones independientes de cada modulo

--- configuracion/urls.py
+++ configuracion/urls.py
@@ -16,7 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from proyecto_principal import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', views.home, name='home'),
 ]
---
+++ productos_proscesados/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class ProductosProscesadosConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'productos_proscesados'
