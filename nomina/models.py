9fc4faa28290f2b50530b35dfb936085f16ac093
Santiago5631 <davidsanti5631@gmail.com>
2025-10-16 10:11:36 -0500

    subiendo el modulo de nomina

--- nomina/models.py
+++ nomina/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class Nomina(models.Model):
+    empleado = models.ForeignKey('empleado.Empleado', on_delete=models.CASCADE)
+    nombre = models.CharField(max_length=100)
+    valor_hora = models.DecimalField(max_digits=10, decimal_places=2)
+    pago = models.DecimalField(max_digits=10, decimal_places=2)
+    admin = models.ForeignKey('administrador.Administrador', on_delete=models.SET_NULL, null=True)
+
+    def __str__(self):
+        return f"{self.nombre} - {self.pago}"
