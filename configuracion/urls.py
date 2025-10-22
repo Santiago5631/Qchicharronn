6751fb450a2572f8a250291f36219791ef6d8b6e
Santiago5631 <davidsanti5631@gmail.com>
2025-10-22 10:05:55 -0500

    commit

--- configuracion/urls.py
+++ configuracion/urls.py
@@ -21,5 +21,6 @@ from proyecto_principal import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.home, name='home'),
+    path('', include('proyecto_principal.urls', namespace='pollo')),
 
 ]
