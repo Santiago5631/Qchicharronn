07c3cbc912aa123e149a60d47bf6504ecd4f4d08
David Puerto <puertobotiadavidalejandromail.com>
2025-10-23 07:38:26 -0500

    arreglon de models.py de unidad para que sean fijos

--- unidad/admin.py
+++ unidad/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Unidad
+
+@admin.register(Unidad)
+class UnidadAdmin(admin.ModelAdmin):
+    list_display = ('nombre', 'descripcion')
+from django.contrib import admin
 
 # Register your models here.
