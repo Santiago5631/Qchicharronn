027ebc827c69f9bc67c02e07ae6e70e5b0816c4b
David Alejandro Puerto Botia <david_apuerto@soy.sena.edu.co>
2025-10-27 10:23:38 -0500

    correcion del formulario de platos y para agregar una database

--- plato/urls.py
+++ plato/urls.py
@@ -8,5 +8,5 @@ path('listar/', PlatoListView.as_view(), name='listar_plato'),
 path('crear/', PlatoCreateView.as_view(), name='crear_plato'),
 path('editar/', PlatoUpdateView.as_view(), name='editar_plato'),
 path('eliminar/', PlatoDeleteView.as_view(), name='eliminar_plato'),
-
+path('ajax/agregar_producto/', agregar_producto_ajax, name='agregar_producto_ajax'),
 ]
--- plato/views.py
+++ plato/views.py
@@ -7,6 +7,10 @@ from django.http import JsonResponse
 from django.views.decorators.csrf import csrf_exempt
 from django.utils.decorators import method_decorator
 from django.contrib import messages
+from django.views.decorators.http import require_POST
+from producto.models import Producto
+from unidad.models import Unidad
+import json
 
 
 # 🔹 Mixin genérico para manejar mensajes de éxito
@@ -53,31 +57,39 @@ class PlatoCreateView(SuccessMessageMixinCustom, CreateView):
     def get(self, request, *args, **kwargs):
         self.object = None
         form = self.get_form()
-        formset = PlatoProductoFormSet(queryset=PlatoProducto.objects.none())
         return render(request, self.template_name, {
             'form': form,
-            'formset': formset,
             'titulo': 'Crear Plato',
-            'entidad': 'Plato'
+            'entidad': 'Plato',
+            'productos': Producto.objects.all(),
+            'unidades': Unidad.objects.all(),
         })
 
     def post(self, request, *args, **kwargs):
-        self.object = None
         form = self.get_form()
-        formset = PlatoProductoFormSet(self.request.POST)
+        productos_json = request.POST.get("productos_json", "[]")
+        productos_data = json.loads(productos_json)
 
-        if form.is_valid() and formset.is_valid():
+        if form.is_valid():
             plato = form.save()
-            formset.instance = plato
-            formset.save()
-            messages.success(request, self.success_message)  # 👈 mensaje automático
+            for p in productos_data:
+                producto = Producto.objects.get(id=p["id"])
+                unidad = Unidad.objects.get(nombre=p["unidad"])
+                PlatoProducto.objects.create(
+                    plato=plato,
+                    producto=producto,
+                    cantidad=p["cantidad"],
+                    unidad=unidad
+                )
+            messages.success(request, self.success_message)
             return redirect(self.success_url)
 
         return render(request, self.template_name, {
             'form': form,
-            'formset': formset,
             'titulo': 'Crear Plato',
-            'entidad': 'Plato'
+            'entidad': 'Plato',
+            'productos': Producto.objects.all(),
+            'unidades': Unidad.objects.all(),
         })
 
 
@@ -130,3 +142,25 @@ class PlatoDeleteView(DeleteView):
         self.object = self.get_object()
         self.object.delete()
         return JsonResponse({"status": "ok"})
+
+
+@require_POST
+@csrf_exempt
+def agregar_producto_ajax(request):
+    producto_id = request.POST.get("producto")
+    cantidad = request.POST.get("cantidad")
+    unidad_id = request.POST.get("unidad")
+
+    try:
+        producto = Producto.objects.get(id=producto_id)
+        unidad = Unidad.objects.get(id=unidad_id)
+
+        data = {
+            "id": producto.id,
+            "nombre": producto.nombre,
+            "cantidad": cantidad,
+            "unidad": unidad.nombre,
+        }
+        return JsonResponse({"status": "ok", "producto": data})
+    except Exception as e:
+        return JsonResponse({"status": "error", "message": str(e)}, status=400)
