346a91a2b90fa9928712e3ddd72dce5f899d7cce
David Alejandro Puerto Botia <david_apuerto@soy.sena.edu.co>
2025-11-24 07:42:30 -0500

    arreglo del modulo de inventario para que muestre por peso la carne papa y demas

--- configuracion/settings.py
+++ configuracion/settings.py
@@ -9,6 +9,8 @@ https://docs.djangoproject.com/en/5.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.2/ref/settings/
 """
+import os
+
 from django.urls import reverse_lazy
 from pathlib import Path
 from decouple import config
